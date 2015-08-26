ccb7f02563d315d7da6f7134a2c49b5741c21bfa
Mitch Rivet & Ben Casalino <student@epicodus.com>
2015-08-26 12:11:56 -0700

    add specs for patron class

---
+++ src/Patron.php
@@ -0,0 +1,82 @@
+<?php
+
+  class Patron
+  {
+    private $name;
+    private $id;
+
+    function __construct($name, $id)
+    {
+      $this->name = $name;
+      $this->id = $id;
+    }
+
+    function setPatronName($new_patron_name)
+    {
+      $this->name = $new_patron_name;
+    }
+
+    function getPatronName()
+    {
+      return $this->name;
+    }
+
+    function getId()
+    {
+      return $this->id;
+    }
+
+    function save()
+    {
+      $GLOBALS['DB']->exec("INSERT INTO patrons (names)
+      VALUES ('{$this->getPatronName()}')");
+      $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function deleteAll()
+    {
+      $GLOBALS['DB']->exec("DELETE FROM patrons;");
+    }
+
+    static function getAll()
+    {
+      $returned_patrons = $GLOBALS['DB']->query("SELECT * FROM patrons;");
+      $patrons = array();
+
+      foreach($returned_patrons as $patron) {
+        $name = $patron['names'];
+        $id = $patron['id'];
+
+        $new_patron = new Patron ($name, $id);
+        array_push($patrons, $new_patron);
+      }
+      return $patrons;
+
+    }
+
+    static function find($search_id)
+    {
+      $found_patron = null;
+      $patrons = Patron::getAll();
+      foreach($patrons as $patron) {
+        $patron_id = $patron->getId();
+        if ($patron_id == $search_id) {
+          $found_patron = $patron;
+        }
+      }
+
+      return $found_patron;
+    }
+
+    function updatePatronName($new_name)
+    {
+      $GLOBALS['DB']->exec("UPDATE patrons SET names = '{$new_name}' WHERE id = {$this->getId()};");
+      $this->setPatronName($new_name);
+    }
+
+    function deletePatron()
+    {
+      $GLOBALS['DB']->exec("DELETE FROM patrons WHERE id = {$this->getId()};");
+    }
+  }
+?>
---
+++ tests/PatronTest.php
@@ -0,0 +1,178 @@
+<?php
+
+
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+  require_once "src/Patron.php";
+
+  $server = 'mysql:host=localhost;dbname=library_test';
+  $username = 'root';
+  $password = 'root';
+  $DB = new PDO($server, $username, $password);
+
+  class PatronTest extends PHPUnit_Framework_TestCase
+
+  {
+      protected function tearDown()
+      {
+          Patron::deleteAll();
+      }
+
+      function test_getname()
+      {
+        //Arrange
+        $name = "Ryan Seacrest";
+        $id = 1;
+        $test_patron = new Patron ($name, $id);
+
+        //Act
+        $result = $test_patron->getPatronName();
+
+
+        //Assert
+        $this->assertEquals($name, $result);
+
+      }
+
+      function test_setPatornName()
+      {
+        //Arrange
+        $name = "Ryan Seacrest";
+        $id = 1;
+        $test_patron = new Patron ($name, $id);
+
+        //Act
+        $test_patron->setPatronName("George W. Bush");
+        $result = $test_patron->getPatronName();
+
+        //Assert
+        $this->assertEquals($result, "George W. Bush");
+      }
+
+      function test_getId()
+      {
+        //Arrange
+        $id = 1;
+        $name = "Aunt Jehmaima";
+        $test_patron = new Patron ($name, $id);
+
+        //Act
+        $result = $test_patron->getId();
+
+        //Assert
+        $this->assertEquals(1, $result);
+
+      }
+
+      function test_save()
+      {
+        //Arrange
+        $name = "Donald Trump";
+        $id = 1;
+        $test_patron = new Patron($name, $id);
+
+        //Act
+        $test_patron->save();
+
+        //Assert
+        $result = Patron::getAll();
+
+        $this->assertEquals($result[0], $test_patron);
+      }
+
+      function test_getAll()
+      {
+        //arrange
+        $name = "Grimey Harry";
+        $id = 1;
+        $test_patron1 = new Patron($name, $id);
+        $test_patron1->save();
+
+        $name2 = "Grimey Harry Jr.";
+        $id2 = 2;
+        $test_patron2 = new patron($name2, $id2);
+        $test_patron2->save();
+
+        //act
+        $result = Patron::getAll();
+
+        //assert
+        $this->assertEquals([$test_patron1, $test_patron2], $result);
+
+      }
+
+      function test_deleteAll()
+      {
+        $name = "Arnold Schwarzenegger";
+        $id = 1;
+        $test_patron1 = new Patron($name, $id);
+        $test_patron1->save();
+
+        $name2 = "Sylvester Stallone";
+        $id2 = 2;
+        $test_patron2 = new Patron($name2, $id2);
+        $test_patron2->save();
+
+        Patron::deleteAll();
+
+        $result = Patron::getAll();
+        $this->assertEquals([], $result);
+      }
+
+      function test_find()
+      {
+        $name = "Steve Brule";
+        $id = 1;
+        $test_patron1 = new Patron ($name, $id);
+        $test_patron1->save();
+
+        $name2 = "Tim Heidecker";
+        $id2 = 2;
+        $test_patron2 = new Patron ($name2, $id2);
+        $test_patron2->save();
+
+        $result = Patron::find($test_patron2->getId());
+
+        $this->assertEquals($test_patron2, $result);
+      }
+
+    function testUpdatePatronName()
+    {
+      $name = "Satan";
+      $id = 1;
+      $test_patron = new Patron($name, $id);
+      $test_patron->save();
+
+      $new_name = "Jesus";
+
+      $test_patron->updatePatronName($new_name);
+
+      $this->assertEquals("Jesus", $test_patron->getPatronName());
+    }
+
+
+    function testDeletePatron()
+    {
+      $name = "Hulk Hogan";
+      $id = 1;
+      $test_patron = new Patron($name, $id);
+      $test_patron->save();
+
+      $name2 = "Kool-aid Man and Randal Savage";
+      $id2 = 2;
+      $test_patron2 = new Patron($name2, $id2);
+      $test_patron2->save();
+
+      $test_patron->deletePatron();
+
+      $this->assertEquals([$test_patron2], Patron::getAll());
+
+    }
+  }
+
+
+
+?>
